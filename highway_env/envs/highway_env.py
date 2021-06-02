b6bcc23e5956af906b531417192811869b58ee1f
shamb0t <shams.methnani@gmail.com>
2021-06-02 12:14:18 +0400

    add lane_change_reward to highway-env

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -82,15 +82,17 @@ class HighwayEnv(AbstractEnv):
         :return: the corresponding reward
         """
         neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
+        lane_change = action == 0 or action == 2
         lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
             else self.vehicle.lane_index[2]
         scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
         reward = \
+            + self.config["lane_change_reward"] * lane_change \
             + self.config["collision_reward"] * self.vehicle.crashed \
             + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
             + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
         reward = utils.lmap(reward,
-                          [self.config["collision_reward"],
+                          [self.config["collision_reward"] + self.config["lane_change_reward"],
                            self.config["high_speed_reward"] + self.config["right_lane_reward"]],
                           [0, 1])
         reward = 0 if not self.vehicle.on_road else reward
